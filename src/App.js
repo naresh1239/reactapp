2acc746a5dc4e99854e482bb8082ec7e882ac593
Naresh kumawat <iamnareshkumawat1@gmail.com>
2022-01-13 15:01:01 +1300

    f

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css"
+// import React, { useEffect } from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home"
+// import Resume from "./pages/Resume"
+// import Aos from "aos";
+// import "aos/dist/aos.css";
+
+function App() {
+
+  return (
+    <>
+   <h1>helo</h1>
+        </>
+  );
+}
+
+export default App;
